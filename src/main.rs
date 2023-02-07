0f6cd5d45ba0fb2e85312b167af3b8ff83574cae
AJTJ <janke.aaron@gmail.com>
2023-02-06 21:09:43 -0600

    added sundaram and working on atkin

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 fn main() {
-    let erat = eras_fn(100);
-    println!("Erat: {:?}", erat);
+    let erat = eratosthenes(100);
+    println!("eratosthenes: {erat:?}");
+
+    let sund = sundaram(100);
+    println!("sundaram: {sund:?}");
 }
 
 // https://www.baeldung.com/cs/prime-number-algorithms
 
 // Sieve of Eratosthenes
-fn eras_fn(target: usize) -> Vec<usize> {
+fn eratosthenes(target: usize) -> Vec<usize> {
     let mut pr_ind = vec![true; target];
 
     let sq = (target as f64).sqrt().floor() as usize;
 
     for n in 2..sq {
-        if pr_ind[n] == true {
+        if pr_ind[n] {
             let mut j = n * n;
 
-            while j <= target - 1 {
+            while j < target {
                 pr_ind[j] = false;
-                j = j + n
+                j += n
             }
         }
     }
@@ -25,10 +28,48 @@ fn eras_fn(target: usize) -> Vec<usize> {
     pr_ind
         .into_iter()
         .enumerate()
-        .filter_map(|(i, b)| if b == true { Some(i) } else { None })
+        .filter_map(|(i, b)| if b { Some(i) } else { None })
         .collect::<Vec<_>>()
 }
 
 // Sieve of Sundaram
 
+fn sundaram(n: usize) -> Vec<usize> {
+    let k = (n - 1) / 2;
+    let mut pr_ind = vec![true; k + 1];
+
+    let sq = (k as f64).sqrt().floor() as usize;
+
+    for i in 1..sq {
+        let mut j = i;
+
+        while i + j + 2 * i * j <= k {
+            pr_ind[i + j + 2 * i * j] = false;
+            j += 1
+        }
+    }
+
+    let true_indices = pr_ind
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, b)| if b { Some(i) } else { None })
+        .collect::<Vec<_>>();
+
+    println!("sundaram, pre doubling: {true_indices:?}");
+
+    true_indices.iter().map(|n| 2 * n + 1).collect()
+}
+
 // Sieve of Atkin
+
+fn atkin(n: usize) -> Vec<usize> {
+    let sieve = vec![1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59];
+
+    let mut pr_ind = vec![true; n];
+
+    let sq = (n as f64).sqrt().floor() as usize;
+
+    for x in 11..sq {}
+
+    unimplemented!()
+}
